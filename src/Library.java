399a46e1286055503481c77593289d07b26e65aa
ChibiaSad <magur1997@gmail.com>
2022-09-04 18:10:26 +0300

    Сделал домашку (усложненная)

---
+++ src/Library.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class Library {
+    private Book[] books;
+    private Book book;
+
+    public Library(int size){
+        this.books = new Book[size];
+    }
+
+    public void printAllLibrary() {
+        for (Book book : books) {
+            if(book == null){
+                return;
+            }
+            System.out.printf("%s: %s: %d\n", book.getAuthor().toString(), book.getName(), book.getYear());
+        }
+    }
+
+    public void addBook(Book book){
+        for (int i = 0; i < books.length; i++) {
+            if(books[i] == null){
+                books[i] = book;
+                return;
+            }
+        }
+        books = Arrays.copyOf(books, books.length + 1);
+        books[books.length - 1] = book;
+    }
+
+    public void bookData(String name){
+        this.book = searchBook(name);
+        System.out.printf("%s by %s was published in %d\n", book.getName(), book.getAuthor().toString(), book.getYear());
+    }
+
+    public void setYear(String name, int year){
+        this.book = searchBook(name);
+        book.setYear(year);
+    }
+
+    private Book searchBook(String name){
+        for (Book book : books) {
+            if(book.getName().equals(name)){
+                return book;
+            }
+        }
+        throw new RuntimeException("В библиотеке нет книги с названием " + name);
+    }
+}
